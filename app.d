35e9e126bbfaa354f4ddd3138997c4d59f68a4da
yamadamotet <60602525+yamadamotet@users.noreply.github.com>
2020-07-08 07:21:00 +0900

    ダイナミック・スコアリングの途中

--- app.d
+++ app.d
@@ -1,18 +1,46 @@
-import std.stdio;
-import std.string;
+import std;
 
-auto input()
-{
-    return readln().chomp();
-}
+alias sread = () => readln.chomp();
+alias lread = () => readln.chomp.to!long();
+alias aryread(T = long) = () => readln.split.to!(T[]);
 
 void main()
 {
-    auto S = input();
-    if ((S == "AAA") || (S == "BBB"))
+    long n, m, q;
+    scan(n, m, q);
+    writeln(n, m, q);
+
+    auto cnt = new long[](m);
+    //どの問題が何人に解かれたのか
+    writeln(cnt);
+    auto x = new long[][](n);
+    //その人がどの問題を解いたのか
+    writeln(query);
+    foreach (i; 0 .. q)
+
     {
-        writeln("No");
-        return;
+        auto s = aryread();
+        writeln(s);
+        if (s[0] == 1)
+        {
+            long ans;
+            foreach (j; x[s[1] - 1])
+            {
+                ans += n - cnt[j];
+            }
+        }
+        else if (s[0] == 2)
+        {
+
+        }
     }
-    writeln("Yes");
+}
+
+void scan(L...)(ref L A)
+{
+    auto l = readln.split;
+    foreach (i, T; L)
+    {
+        A[i] = l[i].to!T;
+    }
 }
